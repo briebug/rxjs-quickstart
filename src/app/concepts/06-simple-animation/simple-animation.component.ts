7f92ba82b5ec0bbb6743750c99f9995e04051dae
Lukas Ruebbelke <lukas.ruebbelke@briebug.com>
2020-07-14 09:53:18 -0700

    update: fixing animation examples

--- src/app/concepts/06-simple-animation/simple-animation.component.ts
+++ src/app/concepts/06-simple-animation/simple-animation.component.ts
@@ -1,32 +1,30 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-
 import { TweenMax } from 'gsap';
 import * as $ from 'jquery';
-
 import { fromEvent } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+const BALL_OFFSET = 50;
+const CURSOR_OFFSET = 20;
+
 @Component({
   selector: 'app-simple-animation',
-  template: `<div #ball class="ball"></div>`
+  template: `<div #ball class="ball"></div>`,
 })
 export class SimpleAnimationComponent implements OnInit {
   @ViewChild('ball') ball;
 
   ngOnInit() {
-    const BALL_OFFSET = 50;
-    const CURSOR_OFFSET = 20;
-
     fromEvent(document, 'click')
-      .pipe(
-        map((event: any) => {
-          const offset = $(event.target).offset();
-          return {
-            x: event.clientX - offset.left - BALL_OFFSET - CURSOR_OFFSET,
-            y: event.pageY - offset.top - BALL_OFFSET - CURSOR_OFFSET
-          };
-        })
-      )
-      .subscribe(props => TweenMax.to(this.ball.nativeElement, 1, props));
+      .pipe(map((e: MouseEvent) => this.generatePosition(e)))
+      .subscribe(position => TweenMax.to(this.ball.nativeElement, 1, position));
+  }
+
+  generatePosition(e: MouseEvent) {
+    const offset = $(e.target).offset();
+    return {
+      x: e.clientX - offset.left - BALL_OFFSET - CURSOR_OFFSET,
+      y: e.pageY - offset.top - BALL_OFFSET - CURSOR_OFFSET,
+    };
   }
 }
