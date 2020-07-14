a2ca2001759021c2d1c8b8a597f9e80752629cca
Lukas Ruebbelke <lukas.ruebbelke@briebug.com>
2020-07-13 17:04:56 -0700

    update: resetting to an updated cli project

--- README.md
+++ README.md
@@ -1,27 +1,16 @@
-# RxjsQuickstart
+# RxJS Quickstart Examples
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.0.
+<p align="center"><img src="src/assets/readme-screenshot.png" alt="RxJS Quickstart"/></p>
 
-## Development server
+## Prerequisites
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+* You will need to have NodeJS and NPM installed from [nodejs.org](https://nodejs.org)
 
-## Code scaffolding
+## Getting Started
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+```
+git clone https://github.com/briebug/rxjs-quickstart.git
+cd rxjs-quickstart
+npm i
+npm start
+```
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,104 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import {
+  CircleComponent,
+  LineComponent,
+  ShotComponent
+} from './shared/components';
+
+import {
+  AnimalService,
+  BooksService,
+  NotificationService,
+  SalesNumbersService
+} from './shared/services';
+
+import {
+  BasicSequenceComponent,
+  MaintainingStateComponent,
+  MergingStreamsComponent,
+  MapToFunctionsComponent,
+  TriggersComponent,
+  StreamOriginComponent,
+  SimpleAnimationComponent,
+  AnimationComponent,
+  EventCommunicationComponent
+} from './concepts';
+
+import {
+  InputComponent,
+  CounterComponent,
+  SlideshowComponent,
+  LocationComponent,
+  MapComponent,
+  AnnotateComponent,
+  DocComponent,
+  GameComponent,
+  SliderComponent,
+  SalesWidgetComponent
+} from './examples';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CircleComponent,
+    LineComponent,
+    ShotComponent,
+    BasicSequenceComponent,
+    MaintainingStateComponent,
+    MergingStreamsComponent,
+    MapToFunctionsComponent,
+    TriggersComponent,
+    StreamOriginComponent,
+    SimpleAnimationComponent,
+    AnimationComponent,
+    InputComponent,
+    CounterComponent,
+    SlideshowComponent,
+    LocationComponent,
+    MapComponent,
+    AnnotateComponent,
+    DocComponent,
+    GameComponent,
+    SliderComponent,
+    SalesWidgetComponent,
+    EventCommunicationComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatSidenavModule,
+    MatSnackBarModule,
+    MatToolbarModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AnimalService,
+    BooksService,
+    NotificationService,
+    SalesNumbersService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
